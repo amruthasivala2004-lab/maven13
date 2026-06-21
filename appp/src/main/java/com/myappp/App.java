fb47457e85bd4997d87bdd15a9721f826be6dc1b
Amrutha <amruthasivala2004@gmail.com>
2026-06-21 22:14:24 +0530

    azure pipeline example

---
+++ appp/src/main/java/com/myappp/App.java
@@ -0,0 +1,13 @@
+package com.myappp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
